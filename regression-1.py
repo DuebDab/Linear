a60a20db13e8f636887c465e9f9d15c30d4514c5
DuebDab <y.sirintip@hotmail.com>
2024-10-02 20:34:10 +0700

    walla

--- regression-1.py
+++ regression-1.py
@@ -1,6 +1,14 @@
 import json
 
-with open('D:\Uni\Year2\Linear\oil.json', 'r') as file:
+with open('D:\\Uni\\Year2\\Linear\\oil.json', 'r') as file:
     oil_data = json.load(file)
     
-print(oil_data)
+oil_prices = [
+    item["Data 3: Regular All Areas All Formulations"]
+    for item in oil_data
+    if any(year in item.get("Back to Contents", "") for year in ["2021", "2022", "2023"])
+    and item["Data 3: Regular All Areas All Formulations"].replace('.', '', 1).isdigit() 
+]
+oil_prices = [float(price) for price in oil_prices]
+print(oil_prices)
+
