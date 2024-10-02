96fd41b75ca075dd045b4cadee5711a35f4ff87b
DuebDab <y.sirintip@hotmail.com>
2024-10-02 20:40:10 +0700

    aaa

--- regression-1.py
+++ regression-1.py
@@ -9,6 +9,10 @@ oil_prices = [
     if any(year in item.get("Back to Contents", "") for year in ["2021", "2022", "2023"])
     and item["Data 3: Regular All Areas All Formulations"].replace('.', '', 1).isdigit() 
 ]
+
 oil_prices = [float(price) for price in oil_prices]
-print(oil_prices)
+
+#--------------------------------------------------------------------------------------#
+
+
 
